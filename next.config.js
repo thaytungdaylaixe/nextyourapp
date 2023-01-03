e45a8c347f4894814eda2c12196465f4461cc292
thaytungdaylaixe <thaytungdaylaixe@gmail.com>
2023-01-03 14:13:13 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+
+const nextConfig = withPWA({
+  pwa: {
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+    runtimeCaching,
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+  env: {
+    BASE_URL_DEV: "http://localhost:3000",
+    BASE_URL_PROD: " https://allapp.vercel.app/",
+  },
+});
+
+module.exports = nextConfig;
