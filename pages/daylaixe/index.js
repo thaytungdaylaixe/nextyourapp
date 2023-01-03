178fac65f17bb0cc86d47fd786c2da220a7edbd4
thaytungdaylaixe <thaytungdaylaixe@gmail.com>
2023-01-03 13:17:42 +0700

    first commit

---
+++ pages/daylaixe/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./styles/Index.module.css";
+
+import Link from "next/link";
+
+import { Inter } from "@next/font/google";
+const inter = Inter({ subsets: ["latin"] });
+
+const Index = () => {
+  return (
+    <main className={styles.main}>
+      <Link href="/" className={styles.card}>
+        <h2 className={inter.className}>Trang chủ</h2>
+        <p className={inter.className}>
+          Find in-depth information about Next.js features and&nbsp;API.
+        </p>
+      </Link>
+    </main>
+  );
+};
+
+export default Index;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
+
 import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
+
+import { Avatar } from "@mui/material";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,16 +19,65 @@ export default function Home() {
         <link rel="icon" href="/daylaixe/favicon.png" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
+        <div className={styles.icon_home_center}>
+          <Avatar
+            sx={{ width: 300, height: 300 }}
             src="/daylaixe/512x512.png"
-            alt="Next.js Logo"
-            width={100}
-            height={100}
-            priority
           />
-          Your App
+        </div>
+
+        <div className={styles.grid}>
+          <Link href="/daylaixe" className={styles.card}>
+            <h2 className={inter.className}>
+              Học lái xe <span>-&gt;</span>
+            </h2>
+            <p className={inter.className}>
+              Find in-depth information about Next.js features and&nbsp;API.
+            </p>
+          </Link>
+
+          <a
+            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={inter.className}>
+              Learn <span>-&gt;</span>
+            </h2>
+            <p className={inter.className}>
+              Learn about Next.js in an interactive course with&nbsp;quizzes!
+            </p>
+          </a>
+
+          <a
+            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={inter.className}>
+              Templates <span>-&gt;</span>
+            </h2>
+            <p className={inter.className}>
+              Discover and deploy boilerplate example Next.js&nbsp;projects.
+            </p>
+          </a>
+
+          <a
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={inter.className}>
+              Deploy <span>-&gt;</span>
+            </h2>
+            <p className={inter.className}>
+              Instantly deploy your Next.js site to a shareable URL
+              with&nbsp;Vercel.
+            </p>
+          </a>
         </div>
       </main>
     </>
