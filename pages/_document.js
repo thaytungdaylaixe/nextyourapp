4078abec4f1839f48a6b0f50187a4e737a6b1bf9
thaytungdaylaixe <thaytungdaylaixe@gmail.com>
2023-01-03 14:15:51 +0700

    first commit

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="description" content="Thầy Tùng - Dạy lái xe" />
+        <link rel="icon" href="/daylaixe/android-chrome-512x512.png" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
