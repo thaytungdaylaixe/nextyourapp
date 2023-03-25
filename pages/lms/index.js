1eaa8ddc5f168f364b25e9a269d4496d0dd380b8
thaytungdaylaixe <thaytungdaylaixe@gmail.com>
2023-03-25 11:26:13 +0700

    first commit

--- pages/lms/index.js
+++ pages/lms/index.js
@@ -1,37 +1,12 @@
 import React from "react";
-import Link from "next/link";
 import styles from "../../styles/Lms.Index.module.css";
-import {
-  Box,
-  List,
-  Divider,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-} from "@mui/material";
 
 import Layout from "./components/Layout";
-import lmsData from "../../store/lmsData.json";
 
 const index = () => {
-  const infoMon = lmsData.infoMon.reverse();
-
   return (
     <Layout>
-      <List style={{ marginTop: "60px" }}>
-        {infoMon.map((mon, index) => (
-          <Link key={index} href={"/lms/" + mon.ma_mon}>
-            <ListItem disablePadding>
-              <ListItemButton>
-                {index + 1}. &nbsp;
-                <ListItemText primary={mon.ten_mon} />
-              </ListItemButton>
-            </ListItem>
-          </Link>
-        ))}
-      </List>
-      <Divider />
+      <div className={styles.index_page}>Index</div>
     </Layout>
   );
 };
